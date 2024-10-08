1df484886c4119096cd9aa8c9bcc9821be653436
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-08 11:27:34 -0400

    04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...toRemove) {
+    let newArray = [];
+    for (i = 0; i < array.length; i++) {
+        
+        if (!toRemove.includes(array[i])) {
+            newArray.push(array[i]);
+            }
+        }
+        return newArray;
+    }
+    
 
 // Do not edit below this line
 module.exports = removeFromArray;
