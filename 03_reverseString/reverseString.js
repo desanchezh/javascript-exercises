97e5daa5363cb0c82b4799efe51aceca82271138
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-07 22:48:18 -0400

    01 and 02 done, 03 almost

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    
+    let letterArray = [];
+    for (const letters of word) {
+        letterArray.push(letters);
+    }
+    const arrayReversed = letterArray.reverse ();
 
+    wordReversedCommas = arrayReversed.toString()
+    
+    let wordReversed = wordReversedCommas.replace(/,/g,"")
+
+    return wordReversed
 };
 
 // Do not edit below this line
