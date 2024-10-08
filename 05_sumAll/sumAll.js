8b0250808b7732c0e60dfa1dbd4f5799b3895dbe
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-08 18:41:18 -0400

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
+    if (numA < 0 || numB < 0){return "ERROR"} 
+    if (!Number.isInteger(numA) || !Number.isInteger(numB)) {return "ERROR"}
+    if (!typeof(numA) === "number" || !typeof(numB) === "number") {return "ERROR"}
+    let numVar = numA;
+    let allNum = [];
+    if (numA < numB) {
+        for (i = numA; i < numB; i++) {
+            let numCurrent = numVar + 1;
+            allNum.push (numCurrent);
+            numVar = numCurrent
+        }
+    } else {
+        for (i = numB; i < numA; i++) {
+            let numCurrent = numVar - 1;
+            allNum.push (numCurrent);
+            numVar = numCurrent;
+        }
+    }
+    allNum.push(numA)
+    let sum = allNum.reduce((a, b) => a + b, 0)
+    return sum;
+}
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
