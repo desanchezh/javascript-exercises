ac23fa4924a72f6b0b3c39e729aea0af8d24fa51
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-08 21:53:52 -0400

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const convertToCelsius = function() {
+
+//ignore the inverted parameter names...i mixed them up the first time and i'm not fixing it
+const convertToCelsius = function(celsius) {
+  let celsiusConverted = ((celsius-32) * (5/9));
+  fahrResult = round(celsiusConverted, 1)
+  return fahrResult
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fahr) {
+  let fahrConverted = ((fahr * (9/5)) + 32);
+  let celsiusResult = round(fahrConverted, 1)
+  return celsiusResult
 };
 
+function round(value, precision) {
+  let multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
