8f031208bd7ee30aa6a8f8e6394be3b531772e94
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-16 23:44:59 -0400

    08 calculator complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  array = []
+	for (i = 0; i < b; i++) {
+    array.push(a)
+  }
+  return array.reduce((total, current) => total * current)
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(a) {
+	array = []
+  if (a === 0) {return 1}
+  else{
+    for (i = 1; i < (a + 1); i++) {
+    array.push(i)
+  }}
+	return array.reduce((total, current) => total * current, 1)
+  }
 
 // Do not edit below this line
 module.exports = {
