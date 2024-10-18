c0740f6c0d390771f20bafc8a2302a63b3da2b78
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-17 21:44:17 -0400

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let arr = word.replace(/[,!. ]/g,'').split('');
+    let arrReversed = arr.slice().reverse().join('').toLowerCase();
+    arr = arr.join('').toLowerCase();
+    result = arr === arrReversed ? true : false;
+    console.log(arr);
+    console.log(arrReversed);
+    return result
 };
+
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
