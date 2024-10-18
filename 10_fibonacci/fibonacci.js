32023d6e516cd29a5b4df59ec9009615b1135224
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-18 00:33:59 -0400

    10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
 
-};
+const fibonacci = function(index) {
+    let fibArray = [0, 1]
+    if (index < 0) {return "OOPS"}
+    else{
+        for (i = 2; i <= index; i++) {
+            fibArray[i] = fibArray[i-1] + fibArray[i-2];
+        }
+        return fibArray[index];
+    }
+    
+    }
 
 // Do not edit below this line
 module.exports = fibonacci;
